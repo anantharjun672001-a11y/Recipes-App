65f3e95e9fbf825679de2286334a1f84ace60764
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-29 19:25:28 +0530

    basic folder structure created

---
+++ index.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import dotenv, { config } from "dotenv";
+
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.status(200).send("Welcome to the Recipes App API!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
