1ef734c593a19c4bf745cf773dd04a7ecb86a6ac
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-29 20:50:57 +0530

    getById created

--- Controllers/recipeController.js
+++ Controllers/recipeController.js
@@ -22,3 +22,18 @@ export const getAllRecipes = async (req,res) => {
         res.status(500).json({message: error.message});
     }
 }
+
+
+//Get By ID
+
+export const getRecipeById = async (req,res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id);
+        if (!recipe) {
+            return res.status(404).json({message: "Recipe not found"});
+        }
+        res.status(200).json(recipe);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
--- Routes/recipeRoute.js
+++ Routes/recipeRoute.js
@@ -1,11 +1,13 @@
 import express from "express";
-import { createRecipe, getAllRecipes } from "../Controllers/recipeController.js";
+import { createRecipe, getAllRecipes, getRecipeById } from "../Controllers/recipeController.js";
 
 const router = express.Router();
 
 router.post("/create",createRecipe);
 
 router.get("/all",getAllRecipes);
+
+router.get("/:id",getRecipeById);
 
 
 export default router;
