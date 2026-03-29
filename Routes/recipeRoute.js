7735656c4464e17d72ad16efc9b14fd4068707e5
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-29 20:47:53 +0530

    getAllRecipes route added

--- Routes/recipeRoute.js
+++ Routes/recipeRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
-import { createRecipe } from "../Controllers/recipeController.js";
+import { createRecipe, getAllRecipes } from "../Controllers/recipeController.js";
 
 const router = express.Router();
 
 router.post("/create",createRecipe);
+
+router.get("/all",getAllRecipes);
 
 
 export default router;
