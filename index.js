a88eace79f5631ec9f0e884595c6e02e567a0f4c
anantharjun672001-a11y <anantharjun672001@gmail.com>
2026-03-29 19:51:01 +0530

    mongodb connected

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-import connectDB from "./config/dbConfig.js";
+import connectDB from "./Database/dbConfig.js";
 
 
 dotenv.config();
